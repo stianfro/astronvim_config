99f8c2b4f56c0be4e787fb1e837020fc1d395d72
Stian Frøystein <stianfroy@gmail.com>
2023-12-05 10:58:36 +0900

    add user config

--- options.lua
+++ options.lua
@@ -2,11 +2,13 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    -- clipboard = "unnamedplus" -- system clipboard
+    clipboard = ""
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
