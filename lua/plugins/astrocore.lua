c35eb77e55a04f7ca57647c961cad04d7fdfee77
Stian Frøystein <stianfroy@gmail.com>
2024-11-26 17:30:32 +0900

    feat: modify astrocode

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -11,11 +11,11 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      autopairs = true,                                 -- enable autopairs at start
+      cmp = true,                                       -- enable completion at start
+      diagnostics_mode = 3,                             -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true,                              -- highlight URLs at start
+      notifications = true,                             -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -24,14 +24,16 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                   -- vim.opt.<key>
         relativenumber = false, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
+        number = true,          -- sets vim.opt.number
+        spell = false,          -- sets vim.opt.spell
+        signcolumn = "auto",    -- sets vim.opt.signcolumn to auto
+        wrap = false,           -- sets vim.opt.wrap
         clipboard = "",
         conceallevel = 2,
+        shiftwidth = 2,
+        tabstop = 2,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -74,6 +76,14 @@ return {
             -- If you want to run `cargo run` specifically on the current file's directory, you might need a more complex function to change the directory accordingly.
           end,
           desc = "Run Cargo on Current File",
+        },
+        ["<F3>"] = {
+          function()
+            -- This will run `go run` in the terminal. Adjust the command as necessary.
+            vim.cmd "split | terminal go run ."
+            -- If you want to run `cargo run` specifically on the current file's directory, you might need a more complex function to change the directory accordingly.
+          end,
+          desc = "Run Go on Current File",
         },
       },
       t = {
