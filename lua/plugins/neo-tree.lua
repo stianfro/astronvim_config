d8c8426ae1028a2c28bf9f42d2b39620fe54f716
Stian Frøystein <stianfroy@gmail.com>
2025-07-16 12:28:37 +0900

    feat: an attempt was made
    didn't work

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      mappings = {
+        n = { ["s"] = false },
+      },
+    },
+  },
+}
